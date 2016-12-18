57842dba59b0189c9b4411d502b8c83feecc6f85
Tushar Debnath <Tushar@Tushars-MacBook-Pro-2.local>
2016-12-18 12:35:03 +0530

    detail with status

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseObjectObservable } from 'angularfire2';
+import { FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 import { ProjectService } from '../firebase-project.service';
 import { StorageService } from '../firebase-storage.service';
 
@@ -15,9 +15,10 @@ export class DetailComponent implements OnInit {
   title: string = 'download';
   path: string;
   name: string;
+  statusSelector: FirebaseListObservable<any>
   constructor(private ps: ProjectService, private ss: StorageService) {
 
-
+this.statusSelector = this.ps.getstatusPath();
   }
   download() {
 
@@ -29,10 +30,10 @@ export class DetailComponent implements OnInit {
     this.project.subscribe(prjkt => {this.path = prjkt.path; this.name = prjkt.name; } );
   }
 
-  update( newName: string, desc: string) {
+  update( newName: string, desc: string, status: string) {
     let newPath = this.path.replace(this.name, newName);
     this.ss.moveFile(this.path, newPath);
-    this.project.update({name: newName, description: desc, path: newPath} );
+    this.project.update({name: newName, description: desc, path: newPath, status: status} );
   }
 
   ngOnInit() {
